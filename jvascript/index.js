880162dc547ee7ee0f7c93f050040f31438ebbf4
Aubreyhet <2930603106@qq.com>
2019-07-27 20:08:28 +0800

    es6

--- jvascript/index.js
+++ jvascript/index.js
@@ -1,13 +1,59 @@
-//获取用户去鼠标位置信息的方法
-var myDivOne = document.getElementById('myDivOne');
-myDivOne.onclick = function(){
-    alert(
-            //获取客户区鼠标坐标位置
-          '客户区坐标位置：X=' + event.clientX + ' ' + 'Y=' + event.clientY
-          //获取页面坐标位置
-          + '页面坐标位置：X=' + event.pageX + ' ' + 'Y=' + event.pageY
-          //获取鼠标相对整个桌面的坐标位置
-          +  '鼠标相对整个桌面的位置坐标：X=' + event.screenX + ' ' + event.screenY
-    )
-}
+// function bar2(x = 2, y = x) {
+//   return [x, y]
+// }
+// bar2()
+
+
+// var x = x;
+// let x = x;
+
+
+// function myfun(){
+//   let a = 10;
+//   var a = 1;
+// }
+// myfun();
+
+
+// function myfun1(){
+//   let a = 10;
+//   let a = 1;
+// }
+// myfun1();
+
+
+// var tmp = new Date();
+// function f(){
+//     console.log(tmp);
+//     if (false){
+//         var tmp = 'hello world!';
+//     }
+// }
+// f(); 
+
+
+// var s = 'hello';
+// for (var i = 0; i < s.length; i++) {
+//     console.log(s[i]);
+// }
+// console.log(i);
+
+
+// function f1() {
+//   let n = 5;
+//   if(true) {
+//       let n = 10;
+//   }
+//   console.log(n);
+// }
+// f1();
+
+
+// const PI = 3.1415;
+// PI = 3;
+// console.log(PI);
+
+
+// const foo;
+
 
